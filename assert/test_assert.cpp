6db8bac2c58f0db441b46a02e7fd246fa53d9e94
wegatron <wegatron@gmail.com>
2014-04-13 15:38:51 +0800

    init src accumulation

---
+++ assert/test_assert.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  fstream zsw_iof;
+  zsw_iof.open("test.txt");
+
+  return 0;
+}
+
---
+++ error/error.h
@@ -0,0 +1,6 @@
+#ifndef _ERROR_H_
+#define _ERROR_H_
+
+#define FILEOPEN_ERROR 1
+#define FILEPARSE_ERROR 2
+#endif
