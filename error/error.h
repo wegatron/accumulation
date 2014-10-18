2d33f830555a4c1105c80b60b5f21cb565a02bf7
wegatron <wegatron@gmail.com>
2014-10-18 13:38:23 +0800

    mv gauss_solver to linear_solver, add CHECK_RETCODE to error.h

--- error/error.h
+++ error/error.h
@@ -1,6 +1,18 @@
 #ifndef _ERROR_H_
 #define _ERROR_H_
 
+#include <iostream>
+
+#define GOOD 0
 #define FILEOPEN_ERROR 1
 #define FILEPARSE_ERROR 2
+
+#define CHECK_RETCODE(ret_code) do{                             \
+    if (ret_code != GOOD) {                                     \
+      std::cerr << "error occured in " << __FILE__ << __LINE__  \
+                << " as ret_code is abnormal!" << std::endl;    \
+      return ret_code;                                          \
+    }                                                           \
+  }while(0)
+
 #endif
