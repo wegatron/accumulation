c28d7c27b459925bf4709d7ed6de284e9bb28968
wegatron <wegatron@gmail.com>
2014-10-28 10:41:39 +0800

    add OPEN_STREAM in error_ctrl.h

--- error_ctrl.h
+++ error_ctrl.h
@@ -4,31 +4,39 @@
 #include <iostream>
 #include <fstream>
 
-namespace zsw{
-
 #define OK 0
 
-#define CHECK_RETCODE(ret_code) do{                             \
-    if (ret_code != OK) {                                       \
-      std::cerr << "# [ ERROR ] Error occured in " << __FILE__ << __LINE__  \
-                << " as ret_code is abnormal!" << std::endl;    \
-      return ret_code;                                          \
-    }                                                           \
+#define CHECK_RETCODE(ret_code) do{                                     \
+    if (ret_code != OK) {                                               \
+      std::cerr << "# [ ERROR ] Error occured in " << __FILE__ << __LINE__ \
+                << " as ret_code is abnormal!" << std::endl;            \
+      return ret_code;                                                  \
+    }                                                                   \
   }while(0)
 
-#define CALL_FUNC(pn) do{                                               \
+#define CALL_FUNC(pn) do{                                              \
     std::cout << "# [ Run: " << #pn << " ]" << std::endl;               \
       if(pn != OK){ std::cerr << "# [ ERROR ] " << #pn << std::endl; return __LINE__; } \
   }while(0)
 
-  inline int open_stream(const string &path, const ios_base::openmode mode, fstream &fs) {
-    using namespace std;
-    fs.open(path, mode);
-    if(!fs || !fs.good()) {
-      cerr << "Could not open file " << string(path) << endl;
-      return 1;
-    }
-  }
-}
 
+
+
+#define CALL_FUNC2(ptr, pn) do {                               \
+    ptr = pn;                                                   \
+    if (ptr == nullptr) {                                       \
+      std::cerr << "# [ ERROR ] " << __FILE__ << __LINE__       \
+                << #pn << " return null." << endl;              \
+        return __LINE__;                                        \
+    }                                                           \
+  }while(0)
+
+
+#define OPEN_STREAM(path, fs, mode) do {                        \
+    fs.open(path, mode);                                        \
+    if(!fs || !fs.good()) {                                     \
+      std::cerr << "Could not open file " << path << std::endl; \
+      return __LINE__;                                          \
+    }                                                           \
+  }while(0)
 #endif /* ERROR_CTRL_H */
