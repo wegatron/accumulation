8a1fb43776ae5f846e162844d45b8064e3a668ba
wegatron <wegatron@gmail.com>
2015-05-20 22:51:54 +0800

    update implicit tools

--- vector_field_deform/implicit_tools.cpp
+++ vector_field_deform/implicit_tools.cpp
@@ -2,17 +2,51 @@
 
 #include <iostream>
 
+#include <Eigen/Dense>
+
 using namespace zsw;
+
+void zsw::SphereDeformTool::calcU(const Eigen::Vector3d &u_dest, Eigen::Vector3d &u0, Eigen::Vector3d &u1)
+{
+  size_t min_ind = 0;
+  if(fabs(u_dest[1]) < fabs(u_dest[min_ind]) ) min_ind = 1;
+  if(fabs(u_dest[2]) < fabs(u_dest[min_ind]) ) min_ind = 2;
+
+  u0[min_ind] = 0;
+  switch(min_ind) {
+  case 0:
+    u0[1] = u_dest[2];
+    u0[2] = -u_dest[1];
+    break;
+  case 1:
+    u0[0] = u_dest[2];
+    u0[2] = -u_dest[0];
+    break;
+  default: // 2
+    u0[1] = u_dest[0];
+    u0[0] = -u_dest[1];
+    break;
+  }
+  u1 = u_dest.cross(u0);
+}
 
 void zsw::SphereDeformTool::updateVectorFieldAndDeform()
 {
-  std::cerr << "Function " << __FUNCTION__ << "in " << __FILE__ << __LINE__  << " haven't implement!!!" << std::endl;
+  std::shared_ptr<VectorField> vf(new VectorField());
+  // generate ex, fx, rx, br set into vf
+  Eigen::Vector3d u[3];
+  u[2] << center_[cur_][0]-center_[!cur_][0], center_[cur_][1]-center_[!cur_][1], center_[cur][2]-center_[!cur_][2]; // vector field's' direction in inner region
+  calcU(u[2], u[0], u[1]);
+  // @Todo
+  std::shared_ptr<Function> ex_func(new LinearScalarField(u[0].data(), center));
 }
 
 void zsw::SphereDeformTool::updateCenter(const double *new_center)
 {
-  std::cerr << "Function " << __FUNCTION__ << "in " << __FILE__ << __LINE__  << " haven't implement!!!" << std::endl;
-  // center_[now][0] = new_center[0];
+  cur_ = !cur_;
+  center_[cur_][0] = new_center[0];
+  center_[cur_][1] = new_center[1];
+  center_[cur_][2] = new_center[2];
 }
 
 void zsw::VfDeformer::loadModel(const std::string& file_path)
--- vector_field_deform/implicit_tools.h
+++ vector_field_deform/implicit_tools.h
@@ -31,14 +31,15 @@ namespace zsw{
       center_[0][0] = center[0]; center_[0][1] = center[1]; center_[0][2] = center[2];
       center_[1][0] = center_[1][1] = center_[1][2] = 0.0;
       r_[0] = ri; r_[1] = ro;
-      now = 1;
+      cur_ = 0;
     }
     void updateVectorFieldAndDeform();
     void updateCenter(const double *new_center);
     void setDeformer(std::shared_ptr<VfDeformer> deformer) { deformer_ = deformer; }
   private:
+    void calcU(const Eigen::Vector3d &u_dest, Eigen::Vector3d &u0, Eigen::Vector3d &u1);
     std::shared_ptr<VfDeformer> deformer_;
-    unsigned short now;
+    unsigned short cur_;
     double center_[2][3];
     double r_[2];
   };
