51b20205b72bf3d467dfd6298faa61270fb9a7c7
wegatron <wegatron@gmail.com>
2015-05-28 22:08:30 +0800

    normal filtering done and testing

--- bilateral_normal_filtering/test/test_mesh.cpp
+++ bilateral_normal_filtering/test/test_mesh.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   jtf::mesh::tri_mesh trimesh("/home/wegatron/tmp/plane_input.obj");
-  std::pair<size_t, size_t> result = trimesh.ea_->query(38, 94);
+  std::pair<size_t, size_t> result = trimesh.ea_->query(137, 9);
   std::cout << result.first << " " << result.second << std::endl;
   return 0;
 }
