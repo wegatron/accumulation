19331b114cbe4ab6af798856d5d6b7fde6d253ee
wegatron <wegatron@gmail.com>
2015-05-28 22:42:59 +0800

    the preivious bug is caused by input obj, from stl vertex haven't merge

--- bilateral_normal_filtering/test/test_mesh.cpp
+++ bilateral_normal_filtering/test/test_mesh.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  jtf::mesh::tri_mesh trimesh("/home/wegatron/tmp/plane_input.obj");
-  std::pair<size_t, size_t> result = trimesh.ea_->query(137, 9);
+  jtf::mesh::tri_mesh trimesh("/home/wegatron/tmp/Tooth_15.obj");
+  std::pair<size_t, size_t> result = trimesh.ea_->query(1049, 1148);
   std::cout << result.first << " " << result.second << std::endl;
   return 0;
 }
