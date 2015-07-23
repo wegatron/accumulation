553404b5cb932b7558803be8ec2d6fd840c79382
wegatron <wegatron@gmail.com>
2015-07-23 09:40:46 +0800

    update datatype for nthvector, test passed

--- zswlib/data_type.h
+++ zswlib/data_type.h
@@ -1,8 +1,9 @@
-#ifndef DATA_TYPE_H
-#define DATA_TYPE_H
+#ifndef data_TYPE_H
+#define data_TYPE_H
 
 #include <list>
 #include <set>
+#include <boost/function.hpp>
 
 namespace zsw
 {
@@ -14,36 +15,36 @@ namespace zsw
       typedef typename std::list<Scalar>::iterator iterator;
       FakeSet() {}
       void initFromSet(const std::set<Scalar> &in_set) {
-        data.resize(in_set.size());
-        std::copy(in_set.begin(), in_set.end(), data.begin());
+        data_.resize(in_set.size());
+        std::copy(in_set.begin(), in_set.end(), data_.begin());
       }
       void insert(Scalar val)
       {
-        std::list<Scalar>::iterator it = data.begin();
-        while(it!=data.end()) {
+        std::list<Scalar>::iterator it = data_.begin();
+        while(it!=data_.end()) {
           if(val == *it) { return; }
           ++it;
         }
-        data.insert(it,val);
+        data_.insert(it,val);
       }
 
       void erase(Scalar val) {
-        for(std::list<Scalar>::iterator it = data.begin();
-            it!=data.end(); ++it) {
+        for(std::list<Scalar>::iterator it = data_.begin();
+            it!=data_.end(); ++it) {
           if(*it == val) {
-            data.erase(it);
+            data_.erase(it);
             break;
           }
         }
       }
 
-      const_iterator cbegin() const { return data.cbegin(); }
-      const_iterator cend() const { return data.cend(); }
-      iterator begin() { return data.begin(); }
-      iterator end() { return data.end(); }
-      size_t size() const { return data.size(); }
+      const_iterator cbegin() const { return data_.cbegin(); }
+      const_iterator cend() const { return data_.cend(); }
+      iterator begin() { return data_.begin(); }
+      iterator end() { return data_.end(); }
+      size_t size() const { return data_.size(); }
     private:
-      std::list<Scalar> data;
+      std::list<Scalar> data_;
     };
 
   template <typename Scalar>
@@ -56,28 +57,29 @@ namespace zsw
       }
       void insert(Scalar v) {
         std::list<Scalar>::reverse_iterator it;
-        for(it=data.rbegin(); it!=data.rend();
+        for(it=data_.rbegin(); it!=data_.rend();
             ++it) {
-          if(!pre_func(v, *it)) { break; }
+          if(!pre_func_(v, *it)) { break; }
         }
-        data.insert(it.base(), v);
-        if(data.size()>n_) { data.pop_back(); }
+        data_.insert(it.base(), v);
+        if(data_.size()>n_) { data_.pop_back(); }
       }
 
-      size_t size() const { return data.size(); }
+      size_t size() const { return data_.size(); }
       zsw::Scalar getIthVal(size_t ith) {
         if(ith >=n_ ) {
-          std::cerr << "can't get " << ith << " elementh, exceed " << n_ << std::endl;
+          std::cerr << "can't get " << ith << "th elemen, exceed " << n_ << std::endl;
         }
-        std::list<Scalar>::iterator it = data.begin();
+        std::list<Scalar>::iterator it = data_.begin();
         for(size_t i=0; i<ith; ++i) { ++it; }
         return *it;
       }
+      const std::list<Scalar> & getData() const { return data_; }
     private:
       size_t n_;
       boost::function<bool (Scalar, Scalar)> pre_func_;
-      std::list<Scalar> data;
+      std::list<Scalar> data_;
     };
 }
 
-#endif /* DATA_TYPE_H */
+#endif /* data_TYPE_H */
